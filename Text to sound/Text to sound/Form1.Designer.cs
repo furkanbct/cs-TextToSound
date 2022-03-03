dc8ec7e9bf581d716f2ff4d741f6583ac9d9b575
I-Doser <49875242+i-doser@users.noreply.github.com>
2022-03-04 01:00:53 +0300

    Add files via upload

---
+++ Text to sound/Text to sound/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+using System;
+
+namespace Text_to_sound
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.downloadAllButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 144);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(64, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add to List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(279, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(153, 147);
+            this.listBox1.TabIndex = 2;
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(276, 221);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 35);
+            this.downloadButton.TabIndex = 3;
+            this.downloadButton.Text = "Download Selected";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.Location = new System.Drawing.Point(293, 165);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 4;
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(314, 192);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 25);
+            this.playButton.TabIndex = 3;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // downloadAllButton
+            // 
+            this.downloadAllButton.Location = new System.Drawing.Point(357, 221);
+            this.downloadAllButton.Name = "downloadAllButton";
+            this.downloadAllButton.Size = new System.Drawing.Size(75, 35);
+            this.downloadAllButton.TabIndex = 3;
+            this.downloadAllButton.Text = "Download All";
+            this.downloadAllButton.UseVisualStyleBackColor = true;
+            this.downloadAllButton.Click += new System.EventHandler(this.downloadAllButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 259);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.downloadAllButton);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Text To Sound";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button downloadAllButton;
+    }
+}
+
---
+++ Text to sound/Text to sound/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace Text_to_sound
+{
+    public partial class Form1 : Form
+    {
+        ChromeOptions options = new ChromeOptions();
+        ChromeDriverService driverService = ChromeDriverService.CreateDefaultService();
+        ChromeDriver session;
+
+        string downloadPath = Application.StartupPath+"\\Downloads";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                listBox1.Items.Add(textBox1.Text + " > " + comboBox1.SelectedItem.ToString());
+                session.FindElement(By.Name("text")).SendKeys(textBox1.Text);
+                var dropDown = session.FindElement(By.Name("voice"));
+                var selectElement = new SelectElement(dropDown);
+                selectElement.SelectByIndex(comboBox1.SelectedIndex);
+                session.FindElement(By.CssSelector("#app > div:nth-child(1) > div > form > div:nth-child(3) > input")).Click();
+                textBox1.Text = null;
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItem != null)
+            {
+                Play(listBox1.Items.Count - listBox1.SelectedIndex);
+            }
+        }
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            Download(listBox1.Items.Count - listBox1.SelectedIndex);
+        }
+        private void downloadAllButton_Click(object sender, EventArgs e)
+        {
+            DownloadAll();
+        }
+        void Download(int index)
+        {
+            session.FindElement(By.CssSelector("#app > div.section.section--colored > div > div:nth-child(" + index.ToString() + ")" + " > div.card__actions > a:nth-child(2)")).Click();
+        }
+        void DownloadAll()
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                int index = listBox1.Items.Count - i;
+                Download(index);
+            }
+        }
+        void Play(int index) 
+        {
+            session.FindElement(By.CssSelector("#app > div.section.section--colored > div > div:nth-child(" + index.ToString() + ")" + " > div.card__actions > a:nth-child(1)")).Click();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(downloadPath))
+            {
+                System.IO.Directory.CreateDirectory(downloadPath);
+            }
+            driverService.HideCommandPromptWindow = true;
+            options.AddArgument("headless");
+            options.AddUserProfilePreference("download.default_directory", downloadPath);
+            session = new ChromeDriver(driverService, options);
+            session.Url = "https://soundoftext.com";
+            SelectElement select = new SelectElement(session.FindElement(By.Name("voice")));
+            foreach (WebElement item in select.Options)
+            {
+                comboBox1.Items.Add(item.Text);
+            }
+            comboBox1.SelectedIndex = comboBox1.FindStringExact("English (United States)");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            session.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            session.Close();
+        }
+    }
+}
